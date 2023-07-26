5ddb1660ece4af75e0e37d55f0bd129e860c4907
jacekfigiel <jacekfigo1988@gmail.com>
2023-07-26 06:15:36 +0100

    first commit core done

---
+++ weather_project/weather_app/views.py
@@ -0,0 +1,46 @@
+import requests
+from decouple import config
+from django.shortcuts import render
+
+
+# Create your views here.
+def index(request):
+    api_key = config('API_KEY')
+    current_weather_url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid={}"
+
+    if request.method == "POST":
+        city1 = request.POST["city1"]
+        city2 = request.POST.get("city2", None)
+
+        weather_data1 = fetch_current_weather(city1, api_key, current_weather_url)
+
+        if city2:
+            weather_data2 = fetch_current_weather(city2, api_key, current_weather_url)
+        else:
+            weather_data2 = None
+
+        context = {
+            "weather_data1": weather_data1,
+            "weather_data2": weather_data2,
+        }
+        return render(request, "weather_app/index.html", context)
+
+    else:
+        return render(request, "weather_app/index.html")
+
+
+def fetch_current_weather(city, api_key, current_weather_url):
+    response = requests.get(current_weather_url.format(city, api_key)).json()
+
+    if 'main' in response:
+        weather_data = {
+            "city": city,
+            "temperature": round(response['main']['temp'] - 273.15, 2),
+            "description": response['weather'][0]['description'],
+            "icon": response['weather'][0]['icon']
+        }
+    else:
+        weather_data = None
+
+    return weather_data
+
